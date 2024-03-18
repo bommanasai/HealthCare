54b7453fdc78047a85345be1b8ede0a00a4899b4
bommanasai <159231899+bommanasai@users.noreply.github.com>
2024-03-18 11:51:27 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -3,4 +3,4 @@ import seaborn as sns
 data = pd.read_csv('healthcare.csv')
 df.info()
 pivot_table = df.pivot_table(index='Blood Group Type', columns='Gender', values='Medical Condition', aggfunc='count')
-sns.heatmap(pivot_table,annot=True,fmt='g')
+sns.heatmap(pivot_table.corr(),annot=True,fmt='g')
